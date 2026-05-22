6cbd9d8b161d74faa37888153651527c0b8513c7
Fatma Gül Demirbaş <78306577+fatmaguldemirbas13@users.noreply.github.com>
2026-05-22 10:31:18 +0200

    Update functıon to male only

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
+"""def load_data():
+    return pd.read_csv('data/titanic.csv')"""
+#Change in utils.py the function, so that only men are returned.
+
 def load_data():
-    return pd.read_csv('data/titanic.csv')
+    data = pd.read_csv('data/titanic.csv')
+    return data[data['sex'] == 'male']
